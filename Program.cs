17e6b7fe465efd458e250c274d9640373fe72ac8
Rasmus Turn Kruse <rasm158w@mail.hansenberg.dk>
2023-09-06 08:54:03 +0200

    Ikkke god

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Page First Page = new Page(Mynotes.VariablePageOne);
+
+Page Second Page = new Page("Min anden side pÅ VEJ TIL BOMBMAN TYCOON");
+
+Page Third Page = new Page("Min tredje side pÅ VEJ TIL BOMBMAN TYCOON");
+
+Chapter Kapitel1 = new Chapter("Variable");
+
+Kapitel1.Page.Add(FirstPage)
+Kapitel1.Page.Add(SecondPage)
+Kapitel1.Page.Add(ThirdPage)
+
+Kapitel1.Read();
+
+Console.WriteLine("Jones har en kæmpe diller");
